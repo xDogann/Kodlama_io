7dd973028c1d354ea502af18c39f44b16d256cca
Abdussamed Dogan <abdussamed.dgn@hotmail.com>
2022-07-21 13:54:01 +0300

    update

--- Day3/MyFinalProject/Core/DataAccess/IEntityRepository.cs
+++ Day3/MyFinalProject/Core/DataAccess/IEntityRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Core.DataAccess
 {
-    public interface IEntityRepository<T> where T : class,IEntity
+    public interface IEntityRepository<T> where T : class, IEntity
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
